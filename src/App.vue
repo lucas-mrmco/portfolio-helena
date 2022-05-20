5f4a8398bd6633e1489692ca012c128de5ae7eae
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-05-20 10:40:20 +0200

    couleur

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <header>
-    <icon-logo />
-    <FilmIcon class="h-5 w-5 fill-blue-300 stroke-blue-600" />
-    <nav>
-      <RouterLink to="/" class="text-blue-600 underline">Accueil</RouterLink>
-    </nav>
+    <Menu>
+      <MenuButton><MenuIcon class="h-8 w-8 stroke-blue-500" /><span class="sr-only">Menu</span></MenuButton>
+      <MenuItems class="flex flex-col">
+        <MenuItem v-slot="{ active }">
+          <a :class="{ 'bg-blue-500 text-white': active }" href="/account-settings"> Account settings </a>
+        </MenuItem>
+        <MenuItem v-slot="{ active }">
+          <RouterLink to="/" class="text-blue-600 underline" :class="{ 'bg-blue-500 text-white': active }">Accueil</RouterLink>
+        </MenuItem>
+        <MenuItem v-slot="{ active }">
+          <RouterLink to="/non-existant" :class="{ 'bg-blue-500 text-white': active }" class="text-blue-600 underline">Autre </RouterLink>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
   </header>
 
   <RouterView />
 </template>
 
 <script>
-import IconLogo from "./components/icons/IconLogo.vue";
-import { FilmIcon } from "@heroicons/vue/outline";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { MenuIcon } from "@heroicons/vue/outline";
 export default {
-  components: { IconLogo, FilmIcon },
+  components: { Menu, MenuButton, MenuItems, MenuItem, MenuIcon },
 };
 </script>
